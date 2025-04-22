04eb748d1623497cc8d6c819a653b2c95f794901
kimgamin2984 <kimgamin2984@gmail.com>
2025-04-22 03:17:44 +0000

    ㅁ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ month = today.month
 day = today.day
 date_str = f"{month:02}월 {day:02}일"
 filename = "Dinner_Menu.pdf"
-st.text(f"조회일: {date_str}")
+
+#st.text(f"조회일: {date_str}")
 
 load_dotenv()
 API_KEY = os.getenv("NEIS_KEY")
@@ -95,7 +96,7 @@ with tab3:
     data = res.json()
     try:
         timetable = data['hisTimetable'][1]['row']
-        timetable_text = "\n".join([f"{period['PERIO']}교시: {period['ITRT_CNTNT']}" for period in timetable])
-        st.text_area("시간표", timetable_text, height=300)
+        for period in timetable:
+            st.text(f"{period['PERIO']}교시: {period['ITRT_CNTNT']}")
     except:
         st.error("시간표 정보를 불러올 수 없어요.")
