b08fd8a4c38ab4dc71b5402df2d77ac72478bffe
kimgamin2984 <kimgamin2984@gmail.com>
2025-04-29 10:31:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,9 @@ SD_SCHUL_CODE = '7240082'
 
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-if 'selected_grade' not in st.session_state:
-    st.session_state.selected_grade = "1"
-if 'selected_class' not in st.session_state:
-    st.session_state.selected_class = "1"
+query_params = st.experimental_get_query_params()
+selected_grade = query_params.get("grade", ["1"])[0]
+selected_class = query_params.get("class_nm", ["1"])[0]
 
 tab1, tab2, tab3 = st.tabs(["중식", "석식", "시간표"])
 
@@ -90,19 +89,10 @@ with tab2:
 with tab3:
     st.markdown("## 오늘 시간표")
 
-    grade = st.selectbox(
-    "학년",
-    ["1", "2", "3"],
-    index=["1", "2", "3"].index(st.session_state.selected_grade) if 'selected_grade' in st.session_state else 0,
-    key="selected_grade"
-)
+    grade = st.selectbox("학년", ["1", "2", "3"], index=["1", "2", "3"].index(selected_grade))
+    class_nm = st.selectbox("반", [str(i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)].index(selected_class))
 
-class_nm = st.selectbox(
-    "반",
-    [str(i) for i in range(1, 10)],
-    index=[str(i) for i in range(1, 10)].index(st.session_state.selected_class) if 'selected_class' in st.session_state else 0,
-    key="selected_class"
-)
+    st.experimental_set_query_params(grade=grade, class_nm=class_nm)
 
     url = 'https://open.neis.go.kr/hub/hisTimetable'
     params = {
