e9a854ebd7fdd0a80c57ad0dc93d78d7cd051db8
kimgamin2984 <kimgamin2984@gmail.com>
2025-04-29 18:10:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,11 +66,11 @@ with tab2:
                                     if len(next_row) > col_idx:
                                         content = next_row[col_idx]
                                         menu_items = content.strip().split("\n")
-                                        st.write(menu_items)
                                         if menu_items == [""]:
                                              st.error(f"{date_str} 석식 식단이 없습니다.")
-                                        for item in menu_items:
-                                            st.markdown(f"- {item.strip()}")
+                                        else:
+                                            for item in menu_items:
+                                                st.markdown(f"- {item.strip()}")
                                         found = True
                                 break
                         if found:
